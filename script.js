77cb36cf90beb9e5000df4afb6506cc83bc40ade
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2026-03-25 01:58:26 +0000

    opa

--- script.js
+++ script.js
@@ -5,19 +5,23 @@ const taskInput = document.querySelector("#taskInput")
 taskBtn.addEventListener("click", function(){
     const li = document.createElement('li');
     const taskInputValue = taskInput.value;
+
     if (taskInputValue == ''){
         return
     }
+
     const span = document.createElement('span');
     span.textContent = taskInputValue;
+
     const button = document.createElement('button');
     button.textContent = 'Excluir'
+
     button.addEventListener('click', function(){
         li.remove();
     })
+
     li.appendChild(span)
     li.appendChild(button)
-    
-    li.textContent = taskInputValue;
+
     taskList.appendChild(li);
 })
